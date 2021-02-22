13a3a3119b1fbf24c9b263abca522e0e1999a3dc
Arsenic <vector2912@gmail.com>
2021-02-22 18:24:37 +0530

    Completed Basic Register

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
+// User Model
+const User = require('../models/user.js');
+
+const salt = 13;
 
 // Login handler
 router.get('/login', (req, res) => {
@@ -12,6 +17,56 @@ router.get('/register', (req, res) => {
 
 // Register handler
 router.post('/register', (req, res) => {
+	console.log(req.body);
+	const { name, email, password, password2 } = req.body;
+	let errors = [];
+	console.log(`Name: ${name}\nEmail: ${email}\nPass: ${password}`);
+
+	if (!name || !email || !password ||  !password2) {
+		errors.push({ msg: 'Please fill in all the fields' });
+	} 
+
+	// Check if match
+	if (password !== password2) {
+		errors.push({ msg: 'Passwords does not match' });
+	}
+
+	// Check if password is more than 6 charcayers
+	if (password.length < 6) {
+		errors.push({ msg: 'Password must be of atleast 6 characters long' });
+	}
+
+	if (errors.length > 0) {
+		res.render('register', { errors, name, email, password, password2 })
+	} else {
+		// validation passed
+		User.findOne({email: email}).exec((err, user) => {
+			console.log(user);
+			if (user) {
+				console.log("EMIL EXISTS")
+				errors.push({ msg: 'Email already registered' });
+				res.render('register', { errors })
+			} else {
+				const newUser = new User( {
+					name,
+					email,
+					password 
+				})
+				// Hash the password
+				bcrypt.hash(newUser.password, salt, (err, hash) => {
+					if (err) throw err;
+					// save pass to hash
+					newUser.password = hash;
+					newUser.save()
+						.then(value => {
+							console.log(value);
+							res.redirect('/users/login');
+						})
+						.catch(err => console.log(err));
+				})
+			}
+		})
+	}
 
 });
 
